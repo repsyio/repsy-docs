6e126aebc58ebf05a52ceefe1c7a41b9871b62bc
firatkucuk <firat@fria.io>
2021-09-16 06:28:15 +0200

    push fix and github url change

--- package.sh
+++ package.sh
@@ -3,4 +3,4 @@ set -e
 
 docker login -u "$DOCKER_HUB_LOGIN" -p "$DOCKER_HUB_TOKEN"
 docker build . --file Dockerfile --tag repsy/repsy-docs:"$1"
-docker push repsy/docs:"$1"
+docker push repsy/repsy-docs:"$1"
